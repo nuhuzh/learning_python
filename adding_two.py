d36075144841da2fbfe4a3b63d7c32f0a8260bf0
nuhuzh <nuhuzakarihamidu222@gmail.com>
2025-05-09 03:00:47 +0900

    Practice converting a numerical string to an integer using type casting

---
+++ adding_two.py
@@ -0,0 +1,9 @@
+# Ask user for two numbers and display their sum.
+
+print("Enter to numbers to check their sum.")
+
+# Use int() to convert numerical strings to integers.
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+print(f"The total is {n1 + n2}")
+
