f82dec8d3f2954c34bec3dfe138d9830b4e20865
nuhuzh <nuhuzakarihamidu222@gmail.com>
2025-05-09 02:09:32 +0900

    Practice accepting user input a little more using input()

---
+++ hello2.py
@@ -0,0 +1,7 @@
+# Ask for user's first and last names and output greeting "Howdy, first_name last_name"
+# In this first part we use two variables in successive input() to get the names
+
+print("Please enter your first name and last names...")
+first_name = input("Your first name: ")
+last_name = input("Your last name: ")
+print(f"Howdy, {first_name} {last_name}")
